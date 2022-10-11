64a02c30f7d833cdd24903a835ceec7b0ee73b0d
Charles <charlesferlet@gmail.com>
2022-10-11 15:59:38 +0200

    style: fix services/club.py

--- services/club.py
+++ services/club.py
@@ -9,18 +9,19 @@ class ClubService():
         club = [club for club in self.clubs if club['email'] == email]
         return {'club': club[0] if self.was_found(club) is True else club,
                 'template': self.login_validation(self.was_found(club)),
-                'message': None if self.was_found(club) is True else "Sorry, that email wasn't found"}
+                'message': None if self.was_found(club) is True
+                else "Sorry, that email wasn't found"}
 
     def get_club_by_name(self, name):
         return [club for club in self.clubs if club['name'] == name][0]
 
-    def was_found(self, club):
+    def was_found(self, club: list) -> bool:
         if club:
             return True
         else:
             return False
 
-    def login_validation(self, club_found = False):
+    def login_validation(self, club_found: bool = False) -> str:
         if club_found is False:
             return 'index.html'
         else:
