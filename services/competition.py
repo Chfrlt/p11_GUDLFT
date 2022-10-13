45f85c991e763261dbf5db664db0f61cbcdb6ac8
Charles <charlesferlet@gmail.com>
2022-10-13 05:36:11 +0200

    feat: Add a function to check if competition was found
    This add an object method was_found()  to the class CompetitionService to prevent raising an error when trying to find a competition with an invalid referential such as a wrong name.

--- services/competition.py
+++ services/competition.py
@@ -7,8 +7,36 @@ class CompetitionService():
     def __init__(self) -> None:
         pass
 
-    def get_competition_by_name(self, name):
-        return [comp for comp in self.get_competitions() if comp['name'] == name][0]
+    def get_competition_by_name(self, name: str) -> Competition | None:
+        """
+        Find corresponding competition from passed name.
+
+        Arg:
+            name (str): The name of the requested competition
+
+        Returns:
+            Competition: Found competition as object if competition was found.
+            None: If competition wasn't found.
+        """
+        comp = [comp for comp in repo_get_competitions()
+                if comp.competition_name == name]
+        return comp[0] if self.was_found(comp) else None
+
+
+    def was_found(self, competition: Competition | None) -> bool:
+        """
+        Check if the passed competition is not None.
+
+        Arg:
+            competition (Competition | None): The competition to check.
+
+        Returns:
+            bool: True if competition is not None, False otherwise.
+        """
+        if competition:
+            return True
+        else:
+            return False
 
     def get_competitions(self):
         """
